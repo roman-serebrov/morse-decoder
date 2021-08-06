7c41c442a81fe499a44a38b7ade611782ec52db8
Roman Serebrov <romanspb00@mail.ru>
2021-08-07 01:35:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -36,11 +36,37 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
-}
-
+    const DOTE = '10'
+    const DASH = '11'
+    let start = 0
+    let count_symbol = 10
+    const symbol = 2
+    const arr = expr.split('')
+    const res = []
+    let item = arr.length / 10
+    while(item > 0) {
+      let strMorse = ''
+      let strArrCode = arr.splice(start, count_symbol)
+      if(strArrCode.join('') === '**********') {
+        item--
+        res.push(' ')
+        continue
+      }
+      for(let i = 0; i<strArrCode.length; i+=symbol) {
+        if(strArrCode.slice(i, i+symbol).join('') === DOTE) {
+          strMorse += '.'
+        }
+        if(strArrCode.slice(i, i+symbol).join('') === DASH) {
+          strMorse += '-'
+        }
+       
+      }
+      res.push(MORSE_TABLE[strMorse])
+      item--
+    }
+      return res.join('')
+    }
 module.exports = {
     decode
 }
